154ddc54c22800ab3ee2e008a788d8eb9254cb2e
Ashar Ali Shafqat <asharalishafqat99@gmail.com>
2025-12-12 11:47:01 +0500

    Initial commit

---
+++ src/main/java/com/ag/gitPracticing/GitPracticingApplication.java
@@ -0,0 +1,13 @@
+package com.ag.gitPracticing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ag/gitPracticing/GitPracticingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.gitPracticing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
